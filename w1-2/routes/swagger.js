9bd0047a96659f52e04141d70d5cd3ed98c004f6
Breanna Russell <brekayussell@gmail.com>
2024-05-07 15:53:10 -0600

    w3 setup

--- w1-2/routes/swagger.js
+++ w1-2/routes/swagger.js
@@ -1,7 +1,7 @@
 // Import Express Router and Swagger-related packages
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');        // Middleware to serve the Swagger UI
-const swaggerDocument = require('../swagger.json');     // Swagger API specification document
+const swaggerDocument = require('../controllers/swagger.json');     // Swagger API specification document
 
 // Serve Swagger UI at '/api-docs'
 router.use('/api-docs', swaggerUi.serve);               // Middleware to serve the files necessary for Swagger UI
